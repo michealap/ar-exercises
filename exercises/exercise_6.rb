723876f44027503957ad46ad2b31286ac41f02ad
Micheala Palmer <michealapalmer@gmail.com>
2022-02-04 21:49:40 -0500

    update one-to-many association between store and employee, insert more records into employees for store 1 and 2

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,12 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Sarah", last_name: "King", hourly_rate: 70)
+@store1.employees.create(first_name: "Tristan", last_name: "Jacobs", hourly_rate: 40)
+@store1.employees.create(first_name: "Rachel", last_name: "Blake", hourly_rate: 60)
+
+@store2.employees.create(first_name: "Michie", last_name: "Lynn", hourly_rate: 50)
+@store2.employees.create(first_name: "Emily", last_name: "Baker", hourly_rate: 65)
+@store2.employees.create(first_name: "Theo", last_name: "Dore", hourly_rate: 35)
+@store2.employees.create(first_name: "Jasmine", last_name: "Young", hourly_rate: 40)
